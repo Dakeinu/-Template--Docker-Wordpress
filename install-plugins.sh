3866dab66a6e7eda848e3a3d6000ae76d89f7f3f
Dakeinu <remi.falcati@ynov.com>
2024-10-22 16:28:14 +0200

    V.1 : Fix FTP and Wordpress plugins

--- install-plugins.sh
+++ install-plugins.sh
@@ -6,10 +6,28 @@ set -e
 docker-entrypoint.sh apache2-foreground &
 
 # Attendre que WordPress soit prêt
-until $(curl --output /dev/null --silent --head --fail http://falcatiremi.com:80); do
+until $(curl --output /dev/null --silent --head --fail http://"$WORDPRESS_DOMAIN":80); do
     printf '.'
     sleep 5
 done
+
+# Vérifier que wp-cli est installé
+if ! command -v wp &> /dev/null
+then
+    echo "wp-cli could not be found, installing..."
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
+
+# Vérifier que WordPress est installé
+until wp core is-installed --allow-root; do
+    printf "En attente que WordPress soit prêt...\n"
+    sleep 5
+done
+
+# Assurer que les permissions sont correctes
+chown -R www-data:www-data /var/www/html
 
 # Installer et activer les plugins
 echo "Installation des plugins..."
@@ -23,7 +41,7 @@ plugins=(
 
 for plugin in "${plugins[@]}"; do
     echo "Installation et activation de $plugin..."
-    wp plugin install "$plugin" --activate --allow-root
+    wp plugin install "$plugin" --activate --allow-root || { echo "Erreur lors de l'installation de $plugin"; exit 1; }
 done
 
 # Mettre à jour la base de données si nécessaire
